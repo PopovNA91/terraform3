87310a3cc338c3c7c5051a10770741d5e66daa7b
PopovNA91 <79901752+PopovNA91@users.noreply.github.com>
2021-10-09 17:04:50 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "bucketnetology"
+    key = "path/to/my/key"
+    region = "us-west-2"
+  }
+}
+
+locals {
+  ubuntu_instance_count = {
+    stage = 1
+    prod = 2
+  }
+  
+  ubuntu_instance_type = {
+    stage = "t3.micro"
+    prod = "t3.large"
+  }
+  
+  type_count = {
+    stage = "t3.micro"
+    prod = "t3.large"
+  }
+}
+
+resource "aws_instance" "Ubuntu" {
+  ami = "ami-03d5c68bab01f3496"
+  instance_type = local.ubuntu_instance_type[terraform.workspace]
+  count = local.ubuntu_instance_count[terraform.workspace]
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    Name = "Ubuntu_netology"
+    Owner = "Popov Nikolay"
+    Project = "Terraform homework"
+  }
+}
+
+resource "aws_instance" "CentOS" {
+  ami = "ami-0686851c4e7b1a8e1"
+  for_each = local.type_count
+  instance_type = each.value
+  tags = {
+    Name = "Ubuntu_netology"
+    Owner = "Popov Nikolay"
+    Project = "Terraform homework"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
